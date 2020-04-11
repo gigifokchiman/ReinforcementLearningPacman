dc25d8e3a101641a01f5e4542d2472f6b5836a6b
chimanfok <caln_10@hotmail.com>
2020-04-11 23:57:10 +0800

    rename folders

---
+++ A1_Search/solveEightQueens.py
@@ -0,0 +1,163 @@
+import random
+import copy
+from optparse import OptionParser
+import util
+import numpy as np
+
+class SolveEightQueens:
+    def __init__(self, numberOfRuns, verbose, lectureExample):
+        """
+        Value 1 indicates the position of queen
+        """
+        self.numberOfRuns = numberOfRuns
+        self.verbose = verbose
+        self.lectureCase = [[]]
+        if lectureExample:
+            self.lectureCase = [
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0],
+            [1, 0, 0, 0, 1, 0, 0, 0],
+            [0, 1, 0, 0, 0, 1, 0, 1],
+            [0, 0, 1, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            ]
+    def solve(self):
+        solutionCounter = 0
+        for i in range(self.numberOfRuns):
+            if self.search(Board(self.lectureCase), self.verbose).getNumberOfAttacks() == 0:
+                solutionCounter += 1
+        print("Solved: %d/%d" % (solutionCounter, self.numberOfRuns))
+
+    def search(self, board, verbose):
+        """
+        Hint: Modify the stop criterion in this function
+        """
+        newBoard = board
+        i = 0
+        con_moves = 0
+        while True:
+            if verbose:
+                print("iteration %d" % i)
+                print(newBoard.toString())
+                print("# attacks: %s" % str(newBoard.getNumberOfAttacks()))
+                print(newBoard.getCostBoard().toString(True))
+            currentNumberOfAttacks = newBoard.getNumberOfAttacks()
+            (newBoard, newNumberOfAttacks, newRow, newCol) = newBoard.getBetterBoard()
+            i += 1
+            if currentNumberOfAttacks < newNumberOfAttacks or newNumberOfAttacks == 0 or i >= 100:
+                break
+
+        return newBoard
+
+class Board:
+    def __init__(self, squareArray = [[]]):
+        if squareArray == [[]]:
+            self.squareArray = self.initBoardWithRandomQueens()
+        else:
+            self.squareArray = squareArray
+
+    @staticmethod
+    def initBoardWithRandomQueens():
+        tmpSquareArray = [[ 0 for i in range(8)] for j in range(8)]
+        for i in range(8):
+            tmpSquareArray[random.randint(0,7)][i] = 1
+        return tmpSquareArray
+          
+    def toString(self, isCostBoard=False):
+        """
+        Transform the Array in Board or cost Board to printable string
+        """
+        s = ""
+        for i in range(8):
+            for j in range(8):
+                if isCostBoard: # Cost board
+                    cost = self.squareArray[i][j]
+                    s = (s + "%3d" % cost) if cost < 9999 else (s + "  q")
+                else: # Board
+                    s = (s + ". ") if self.squareArray[i][j] == 0 else (s + "q ")
+            s += "\n"
+        return s 
+
+    def getCostBoard(self):
+        """
+        First Initalize all the cost as 9999. 
+        After filling, the position with 9999 cost indicating the position of queen.
+        """
+        costBoard = Board([[ 9999 for i in range(8)] for j in range(8)])
+        for r in range(8):
+            for c in range(8):
+                if self.squareArray[r][c] == 1:
+                    for rr in range(8):
+                        if rr != r:
+                            testboard = copy.deepcopy(self)
+                            testboard.squareArray[r][c] = 0
+                            testboard.squareArray[rr][c] = 1
+                            costBoard.squareArray[rr][c] = testboard.getNumberOfAttacks()
+        return costBoard
+
+    def getBetterBoard(self):
+        """
+        "*** YOUR CODE HERE ***"
+        This function should return a tuple containing containing four values
+        the new Board object, the new number of attacks, 
+        the Column and Row of the new queen  
+        For exmaple: 
+            return (betterBoard, minNumOfAttack, newRow, newCol)
+        The datatype of minNumOfAttack, newRow and newCol should be int
+        """
+
+        costboard = self.getCostBoard().toString(True)
+        rows = costboard.split("\n")
+        rows = [r.strip().split() for r in rows if r.strip().split()]
+        np_arr = np.asarray(rows)
+        np_arr[np_arr == "q"] = 999
+        np_arr = np_arr.astype(np.int)
+        minNumOfAttack = int(np.min(np_arr[np_arr != 999]))
+        row_idx, col_idx = np.where(np_arr == minNumOfAttack)
+
+        pick = np.random.randint(len(row_idx), size=1)
+
+        new_row = int(row_idx[pick][0])
+        new_col = int(col_idx[pick][0])
+        betterBoard = copy.deepcopy(self.squareArray)
+
+        for i in range(8):
+            betterBoard[i][new_col] = 0
+
+        betterBoard[new_row][new_col] = 1
+
+        return (Board(betterBoard), minNumOfAttack, new_row, new_col)
+
+        # util.raiseNotDefined()
+
+    def getNumberOfAttacks(self):
+        """
+        "*** YOUR CODE HERE ***"
+        This function should return the number of attacks of the current board
+        The datatype of the return value should be int
+        """
+
+        n = len(self.squareArray)
+        sum1 = sum([((sum(i)-1) * sum(i))/2 for i in zip(*self.squareArray)])
+        sum2 = sum([((sum(i)-1) * sum(i))/2 for i in self.squareArray])
+        shift_1 = [[0]*i + val + [0]*(n-i-1) for i, val in enumerate(self.squareArray)]
+        sum3 = sum([((sum(i)-1) * sum(i))/2 for i in zip(*shift_1)])
+        shift_2 = [[0]*(n-i-1) + val + [0]*i for i, val in enumerate(self.squareArray)]
+        sum4 = sum([((sum(i)-1) * sum(i))/2 for i in zip(*shift_2)])
+
+        return int(sum1 + sum2 + sum3 + sum4)
+
+        # util.raiseNotDefined()
+
+if __name__ == "__main__":
+    #Enable the following line to generate the same random numbers (useful for debugging)
+    random.seed(1)
+    parser = OptionParser()
+    parser.add_option("-q", dest="verbose", action="store_false", default=True)
+    parser.add_option("-l", dest="lectureExample", action="store_true", default=False)
+    parser.add_option("-n", dest="numberOfRuns", default=1, type="int")
+    (options, args) = parser.parse_args()
+    EightQueensAgent = SolveEightQueens(verbose=options.verbose, numberOfRuns=options.numberOfRuns, lectureExample=options.lectureExample)
+    EightQueensAgent.solve()
